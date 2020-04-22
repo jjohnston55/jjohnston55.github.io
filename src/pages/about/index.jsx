449d6db62199f4ce7d2b35e7f7243b21d7909f92
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-04-21 20:50:43 -0600

    Added more to about section

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -14,8 +14,15 @@ const About = () => {
                     </span>
                 </Col>
             </Row>
+            <br />
             <Row>
                 <Col>
+                    <span>
+                        I acquire new tasks easily and have maintained Dean’s List (Honour’s) Standing at NAIT. I received positive performance reviews from past employers, and I believe I can further contribute to your team. 
+                        I am hard-working and dependable and I understand the importance of confidentiality and professionalism in the workplace. I am able to work independently or as an enthusiastic and contributing member to a team. 
+                        My previous work terms taught me valuable employee interaction skills and I developed the ability to effectively communicate with fellow staff. 
+                        I possess strong problem solving skills, in both the academic and work environment and do what it takes to get the job done, even under trying circumstances.
+                    </span>
                 </Col>
             </Row>
         </>
