363b67867c97bb2e9a4aa8133d340c0ff0579e1a
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-09-11 10:14:29 -0600

    Added About page header

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -5,18 +5,21 @@ import './index.css';
 
 const About = () => {
     return (
-        <Row id="about" justify='space-around' align='middle'>
-            <Col span={8}>
-                <span>
-                    My name is Jeff Johnston and I am a Full Stack Developer based in Edmonton, Alberta.
-                    I am passionate about learning new technologies and strive to continue expanding my skillset.
-                    When I am not developing, I spend my time with friends and family, snowboarding, and camping.
-                </span>
-            </Col>
-            <Col>
-                <Skills />
-            </Col>
-        </Row>
+        <div id='about'>
+            <h1>About</h1>
+            <Row justify='space-around' align='middle'>
+                <Col span={8}>
+                    <span>
+                        My name is Jeff Johnston and I am a Full Stack Developer based in Edmonton, Alberta.
+                        I am passionate about learning new technologies and strive to continue expanding my skillset.
+                        When I am not developing, I spend my time with friends and family, snowboarding, and camping.
+                    </span>
+                </Col>
+                <Col span={12}>
+                    <Skills />
+                </Col>
+            </Row>
+        </div>
     )
 }
 
