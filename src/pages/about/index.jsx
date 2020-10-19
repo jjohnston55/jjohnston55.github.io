47650e1b70f7bf232f21097fcddf75af336cce0e
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-10-19 12:21:19 -0600

    Added content to about me section

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -6,16 +6,22 @@ import './index.css';
 const About = () => {
     return (
         <div id='about'>
-            <h1>About</h1>
-            <Row justify='space-around' align='middle'>
-                <Col span={8}>
+            <Row className='title' justify='center'>
+                <h1>My name is Jeff Johnston</h1>
+            </Row>
+            <Row className='description' justify='center'>
+                <Col span={12}>
                     <span>
-                        My name is Jeff Johnston and I am a Full Stack Developer based in Edmonton, Alberta.
+                        I am a Full Stack Developer based in Edmonton, Alberta.
+                        I am currently in the <a href='https://www.nait.ca/programs/baist-information-systems' target='_blank' rel='noopener noreferrer'>Bachelor of Applied Information Systems Technology</a> program at the 
+                        Northern Alberta Institute of Technology and have a Diploma in <a href='https://www.nait.ca/programs/dmit-computer-software-development' target='_blank' rel='noopener noreferrer'>Digital Media and IT</a> from the same institute.
                         I am passionate about learning new technologies and strive to continue expanding my skillset.
-                        When I am not developing, I spend my time with friends and family, snowboarding, and camping.
+                        When I am not developing, I like to spend my time with friends and family, snowboarding, and camping.
                     </span>
                 </Col>
-                <Col span={12}>
+            </Row>
+            <Row justify='space-around' align='middle'>                
+                <Col>
                     <Skills />
                 </Col>
             </Row>
