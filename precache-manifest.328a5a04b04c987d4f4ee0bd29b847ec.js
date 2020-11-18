fda5e470a5635d4f45c29ff0600b1a4f8141e0df
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-11-17 23:53:47 -0700

    Updates

---
+++ precache-manifest.328a5a04b04c987d4f4ee0bd29b847ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3060f74a8a5e9eb99c1d4a6bcd2c636e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f88bb8ecd12cb6e3e7d",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "bc1bdfe13ca0deb1b700",
+    "url": "/static/css/main.b165132b.chunk.css"
+  },
+  {
+    "revision": "9f88bb8ecd12cb6e3e7d",
+    "url": "/static/js/2.9dda3910.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/static/js/2.9dda3910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1bdfe13ca0deb1b700",
+    "url": "/static/js/main.2fcc6cbc.chunk.js"
+  },
+  {
+    "revision": "6c3bbc6fe625c1d16a7d",
+    "url": "/static/js/runtime-main.e88fa045.js"
+  },
+  {
+    "revision": "ae687f0342b225c6d8536019750108ea",
+    "url": "/static/media/Jeff Johnston Resume.ae687f03.pdf"
+  },
+  {
+    "revision": "0b8daba2070a1a4b544f64957d2de94d",
+    "url": "/static/media/chatroom.0b8daba2.webm"
+  },
+  {
+    "revision": "965959802690d0edca694abd3d9b5f46",
+    "url": "/static/media/chatroom.96595980.png"
+  },
+  {
+    "revision": "dfe05cb193f178251b350c09151254fb",
+    "url": "/static/media/nhl_stats.dfe05cb1.png"
+  },
+  {
+    "revision": "cbbeb825a392ec1bf667669675c6da11",
+    "url": "/static/media/poverty_sim.cbbeb825.png"
+  }
+]);
